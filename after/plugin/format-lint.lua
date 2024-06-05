383bda340d7e6052e7361e600e2182d19e995603
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2024-06-05 09:10:24 -0300

    i dont know

--- after/plugin/format-lint.lua
+++ after/plugin/format-lint.lua
@@ -1,47 +1,47 @@
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        svelte = { "prettierd" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        json = { "prettierd" },
-        yaml = { "prettierd" },
-        markdown = { "prettierd" },
-        graphql = { "prettierd" },
-        lua = { "stylua" },
-        go = { "goimports", "gofmt" },
-        elixir = { "lexical" },
-        xml = { "prettierd" },
-        -- python = { "isort", "black" },
-    },
-    format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 500,
-    },
+	formatters_by_ft = {
+		javascript = { "prettierd" },
+		typescript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		svelte = { "prettierd" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		json = { "prettierd" },
+		yaml = { "prettierd" },
+		markdown = { "prettierd" },
+		graphql = { "prettierd" },
+		lua = { "stylua" },
+		go = { "goimports", "gofmt" },
+		elixir = { "lexical" },
+		xml = { "prettierd" },
+		-- python = { "isort", "black" },
+	},
+	format_on_save = {
+		lsp_fallback = false,
+		timeout_ms = 500,
+	},
 })
 -- Save without format
 vim.keymap.set({ "n", "v" }, "<leader>s", function()
-    vim.cmd([[:noautocmd w]])
+	vim.cmd([[:noautocmd w]])
 end)
 
-local lint = require("lint")
+-- local lint = require("lint")
 
-lint.linters_by_ft = {
-    javascript = { "eslint_d" },
-    typescript = { "eslint_d" },
-    javascriptreact = { "eslint_d" },
-    typescriptreact = { "eslint_d" },
-    svelte = { "eslint_d" },
-    -- python = { "pylint" },
-}
+-- lint.linters_by_ft = {
+--     javascript = { "eslint_d" },
+--     typescript = { "eslint_d" },
+--     javascriptreact = { "eslint_d" },
+--     typescriptreact = { "eslint_d" },
+--     svelte = { "eslint_d" },
+--     -- python = { "pylint" },
+-- }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    callback = function()
-        require("lint").try_lint()
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--     callback = function()
+--         require("lint").try_lint()
+--     end,
+-- })
