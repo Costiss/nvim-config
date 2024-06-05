9404f34d5904b5f3f0359bc0f177d14c2eba0b1c
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2024-06-05 09:21:42 -0300

    removing old bindings

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,7 @@
-if vim.g.vscode then
-    -- VSCode extension
-else
-    function ColorGlobal(color)
-        color = color or "tokyonight-storm"
-        vim.cmd.colorscheme(color)
-    end
-
-    ColorGlobal()
-    -- ordinary Neovim
+function ColorGlobal(color)
+	color = color or "tokyonight-storm"
+	vim.cmd.colorscheme(color)
 end
+
+ColorGlobal()
+-- ordinary Neovim
--- lua/costis/init.lua
+++ lua/costis/init.lua
@@ -1,21 +1,5 @@
 require("costis.remap")
 require("costis.set")
-if vim.g.vscode then
-	vim.g.codeium_enabled = false
-else
-	vim.api.nvim_set_keymap("n", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
 
-	vim.api.nvim_set_keymap("x", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
-end
-
-return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.colorscheme.catppuccin" },
-	{
-		import = "astrocommunity.editing-support.refactoring-nvim",
-	},
-	{ import = "astrocommunity.pack.java" },
-	{
-		import = "astrocommunity.pack.kotlin",
-	},
-}
+vim.api.nvim_set_keymap("n", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
