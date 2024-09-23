a90929f2b63825fdb88e71d780a12e4679941408
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2024-09-23 17:21:40 -0300

    flutter tools + Json cmd

--- lua/costis/init.lua
+++ lua/costis/init.lua
@@ -3,3 +3,7 @@ require("costis.set")
 
 vim.api.nvim_set_keymap("n", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command("Json", function()
+	vim.cmd("set filetype=json")
+end, {})
