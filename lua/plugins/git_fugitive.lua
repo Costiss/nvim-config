9f844ae3433988872c0355ecefb99a2a715b2ec3
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-04-15 14:50:54 -0300

    feat: moved git status at most right vertial panel

--- lua/plugins/git_fugitive.lua
+++ lua/plugins/git_fugitive.lua
@@ -2,7 +2,14 @@ return {
 	{
 		"tpope/vim-fugitive",
 		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+			vim.keymap.set("n", "<leader>gs", function()
+				vim.cmd.Git({
+					mods = {
+						vertical = true,
+						split = "aboveleft",
+					},
+				})
+			end)
 		end,
 	},
 }
