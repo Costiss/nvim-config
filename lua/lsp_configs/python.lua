640f0d8fedb4bcfe3558e549ac3812e6363cef81
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-04-25 17:32:56 -0300

    chanfed python lsp to basedpyright

--- lua/lsp_configs/python.lua
+++ lua/lsp_configs/python.lua
@@ -5,16 +5,15 @@ local mason = require("mason")
 return function(on_attach)
 	mason.setup({
 		ensure_installed = {
-			"black",
-			"pylsp",
+			"basedpyright",
 		},
 	})
+	--
+	-- conform.formatters_by_ft.python = {
+	-- 	"black",
+	-- }
 
-	conform.formatters_by_ft.python = {
-		"black",
-	}
-
-	lspconfig.pylsp.setup({
+	lspconfig.basedpyright.setup({
 		on_attach = on_attach,
 	})
 end
