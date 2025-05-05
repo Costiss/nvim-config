8057ead995b81ad1f8f4d4e5203058bf8f9901ff
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2025-05-05 20:48:27 -0300

    fixed markdown preview

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,11 +1,13 @@
 return {
 	{
-		-- cd ~/.local/share/nvim/site/pack/packer/start/
-		-- git clone https://github.com/iamcco/markdown-preview.nvim.git
-		-- cd markdown-preview.nvim
-		-- export NODE_OPTIONS=--openssl-legacy-provider
-		-- npx --yes yarn install
-		-- npx --yes yarn build
 		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		-- cd ~/.local/share/nvim/lazy/markdown-preview.nvim/app
+		-- npm i
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
 }
