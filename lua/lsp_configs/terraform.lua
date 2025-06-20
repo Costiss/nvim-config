5d7f332b82df3d5d73caf19304ba90e7e058f145
Gabriel Ganssin <gabriel.ganassin@contabilizei.com.br>
2025-06-20 14:05:28 -0300

    fix: corrected import

--- lua/lsp_configs/terraform.lua
+++ lua/lsp_configs/terraform.lua
@@ -1,11 +1,11 @@
 local lspconfig = require("lspconfig")
--- local mason_install = require("costis.helpers.mason_install")
+local mason_install = require("lsp_configs.helpers.mason_install")
 
 return function(on_attach)
-	-- mason_install({
-	-- 	"terraform-ls",
-	-- 	"tflint",
-	-- })
+	mason_install({
+		"terraform-ls",
+		"tflint",
+	})
 
 	lspconfig.terraformls.setup({
 		on_attach = on_attach,
