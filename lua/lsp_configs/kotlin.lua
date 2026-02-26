8d7f36f6e6f7477f9fc5f747adcbc412764878a2
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2026-02-26 11:10:08 -0300

    feat(kotlin): switch to kotlin-lsp and update config
    Replaced the deprecated kotlin-language-server with kotlin-lsp for improved
    support. Updated the LSP configuration to use the new server, set JVM target
    to 17, and specified JAVA_HOME for the environment. Adjusted root markers and
    enabled the new server for Kotlin files.

--- lua/lsp_configs/kotlin.lua
+++ lua/lsp_configs/kotlin.lua
@@ -1,17 +1,42 @@
-local lspconfig = require("lspconfig")
 local conform = require("conform")
 local mason_install = require("lsp_configs.helpers.mason_install")
 
 return function()
 	mason_install({
-		"kotlin-language-server",
+		"kotlin-lsp",
 	})
 
 	conform.formatters_by_ft.kotlin = {
 		"ktlint",
 	}
 
-	vim.lsp.config("kotlin_language_server", {})
+	-- Define or override the configuration
+	vim.lsp.config("kotlin_lsp", {
+		cmd = { "kotlin-lsp", "--stdio" },
+		filetypes = { "kotlin" },
+		cmd_env = {
+			JAVA_HOME = vim.fn.expand("$HOME/.sdkman/candidates/java/17.0.14-tem"),
+		},
+		root_markers = {
+			"settings.gradle.kts",
+			"settings.gradle",
+			"build.gradle.kts",
+			"build.gradle",
+		},
+		-- Optional: add specific initialization settings
+		settings = {
+			kotlin = {
+				compiler = {
+					jvm = {
+						target = "17", -- Set your project's JVM target
+					},
+				},
+			},
+		},
+	})
 
-	vim.lsp.enable("kotlin_language_server")
+	-- Enable it for the current session
+	vim.lsp.enable("kotlin_lsp")
+
+	vim.lsp.enable("kotlin-lsp")
 end
